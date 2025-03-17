66d887e7a1116085d85b899bfe5e85738f45ff25
Pierre-Antoine Champin <pierre-antoine@w3.org>
2025-03-17 19:31:14 +0100

    improve benchmark, publish Criterion report, and summary in README

--- README.md
+++ README.md
@@ -4,9 +4,65 @@ To use, run `cargo bench`.
 
 This compares performances of
 
-* [iref](https://crates.io/crates/iref)
-* [oxiri](https://crates.io/crates/oxiri)
-* [sophia_iri](https://crates.io/crates/sophia_iri)
+* [iri-string]
+* [iref]
+* [oxiri]
+* [sophia_iri]
 
-in parsing absolute and relative IRIs,
-and in resolving relative IRIs against a base.
+[iref]: https://crates.io/crates/iref
+[iri-string]: https://crates.io/crates/iri-string
+[oxiri]: https://crates.io/crates/oxiri
+[sophia_iri]: https://crates.io/crates/sophia_iri
+
+## Analysis
+
+The detailed results are available [here](https://pchampin.github.io/bench_iri/report/).
+
+For **parsing (absolute) IRIs**, [sophia_iri] is by far the fastest, and [oxiri] is the slowest.
+[oxiri] being slow is probably due to the fact that it does not only check that the underlying text complies with RFC 3987,
+it also populates a data structure representing the different parts of the IRI.
+As we will see, this pays off for IRI resolution,
+but is needlessly costly for the simple cases where we only want to check that an IRI is valid
+(arguably a much more common use-case in RDF).
+[sophia_iri] uses a big [regular expression](https://github.com/pchampin/sophia_rs/blob/af11895647feaa980108d2584c04d77c46764069/iri/src/_regex.rs#L47)
+and benefits from the very good performances of the [regex](https://crates.io/regex) crate.
+Granted, such a huge regular expression is hard to maintain, but the specification of IRIs is not expected to change often.
+So the maintenance cost is acceptable compared to the gain in performance.
+
+For **parsing arbitrary IRI references** (which can therefore be absolute or relative),
+we see a big difference when the input is an absolute IRI reference or a relative one.
+In the former case, the performances are very similar to the case where an (absolute) IRI is expected (described above).
+However, in the latter case, [sophia_iri] drops from being the fastest to being the slowest.
+I have not fully investigated this,
+but one reason might be that [`sophia_iri`] first tries to match a regular expression for absolute IRI references,
+and if it fails, tries to match another regular expression for relative IRI references.
+It might be intersting to try this in the reverse order (hoping that the regular expression for relative references fails faster),
+or combining them into a single regular expression.
+
+For **resolving relative IRI references** against a fixed base, there are two dimensions along which the different crates differ:
+* [iri-string], [oxiri] and [sophia_iri] provide a dedicated type for the base IRI, which pre-parses the base IRI once and for all
+  (actually, [oxiri] uses only one type for all IRIs, but as we saw above, this type always stores the internal structure of the IRI).
+  On the contrary, [iref] uses a standard `Iri` as the base, and must rediscover its internal structure at each resolution.
+  As expected, [iref] is much slower than the others when resolving multiple relative IRI references against the same base.
+* [iri-string] and [iref] forcibly allocate a new buffer for the resolved IRI,
+  while [oxiri] and [sophia_iri] can write the result of the resolution in a pre-allocated buffer.
+  This allows users to reuse the same buffer when resolving multiple relative IRI references, which also saves time.
+  For a fair comparison, [oxiri] and [sophia_iri] have been tested once with a new buffer on each resolution,
+  and once with a mutualized buffer (suffix `Mbuf`).
+  The second set of tests shows significantly better performances.
+*NB: the performances of [oxiri] and [sophia_iri] are the same, for the simple reason that [sophia_iri] is using [oxiri] under the hood for base IRIs.*
+
+
+## Lessons learned
+
+In RDF, the IRIs present in a graph are unlikely to serve as bases to resolve relative IRI references,
+so "common-use" should only wrap the underlying text with the guarantee that it is a valid IRI (or IRI reference).
+
+For such "lightweight" IRIs, the [regex](https://crates.io/regex) crates seems to provide very good performances for parsing
+(with the caveat of relative IRIs in [sophia_iri], to investigate).
+
+For those IRIs that are destined to serve as base (typically, the IRI of a document from which triples or quads are parsed),
+a dedicated type that stores the internal structure of the IRI should be provided
+(all the more that, in these use-cases, the same base is expected to be used multiple times).
+
+Resolving an IRI reference into a pre-allocated buffer should also be supported, to ensure good performances.
--- benches/my_benchmark.rs
+++ benches/my_benchmark.rs
@@ -1,10 +1,9 @@
 use criterion::{black_box, criterion_group, criterion_main, Criterion};
-use oxiri::{Iri as Oxiri, IriRef as OxiriRef};
-use sophia_iri::*;
 use iri_string::format::ToDedicatedString;
+use oxiri::{Iri as Oxiri, IriRef as OxiriRef};
 
-fn abs_examples() -> &'static [&'static str] {
-    &[
+fn abs_examples() -> impl Iterator<Item=&'static str> {
+    [
         "file://foo",
         "ftp://ftp.is.co.za/rfc/rfc1808.txt",
         "http://www.ietf.org/rfc/rfc2396.txt",
@@ -29,91 +28,11 @@ fn abs_examples() -> &'static [&'static str] {
         "http://example.com/foo/bar?q=1&r=2#toto",
         "http://example.com/foo/bar/?q=1&r=2#toto",
         "http://example.com/foo/bar/.././baz",
-    ]
+    ].iter().copied()
 }
 
-fn bench_oxiri_parse(c: &mut Criterion) {
-    c.bench_function("parse abs oxiri", |b| {
-        b.iter(|| {
-            for iri in abs_examples().iter() {
-                black_box(Oxiri::parse(*iri).unwrap());
-            }
-        })
-    });
-}
-
-fn bench_oxiri_parse_relative(c: &mut Criterion) {
-    c.bench_function("parse rel oxiri", |b| {
-        b.iter(|| {
-            for iri in abs_examples().iter() {
-                black_box(OxiriRef::parse(*iri).unwrap());
-            }
-        })
-    });
-}
-
-fn bench_sophia_iri_parse(c: &mut Criterion) {
-    c.bench_function("parse abs sophia", |b| {
-        b.iter(|| {
-            for iri in abs_examples().iter() {
-                black_box(Iri::new(*iri).unwrap());
-            }
-        })
-    });
-}
-
-fn bench_sophia_iri_parse_relative(c: &mut Criterion) {
-    c.bench_function("parse rel sophia", |b| {
-        b.iter(|| {
-            for iri in abs_examples().iter() {
-                black_box(IriRef::new(*iri).unwrap());
-            }
-        })
-    });
-}
-
-fn bench_iristring_parse(c: &mut Criterion) {
-    c.bench_function("parse abs iri-string", |b| {
-        b.iter(|| {
-            for iri in abs_examples().iter() {
-                black_box(iri_string::types::IriStr::new(*iri).unwrap());
-            }
-        })
-    });
-}
-
-fn bench_iristring_parse_relative(c: &mut Criterion) {
-    c.bench_function("parse rel iri-string", |b| {
-        b.iter(|| {
-            for iri in abs_examples().iter() {
-                black_box(iri_string::types::IriReferenceStr::new(*iri).unwrap());
-            }
-        })
-    });
-}
-
-fn bench_iref_parse(c: &mut Criterion) {
-    c.bench_function("parse abs iref", |b| {
-        b.iter(|| {
-            for iri in abs_examples().iter() {
-                black_box(iref::Iri::new(*iri).unwrap());
-            }
-        })
-    });
-}
-
-fn bench_iref_parse_relative(c: &mut Criterion) {
-    c.bench_function("parse rel iref", |b| {
-        b.iter(|| {
-            for iri in abs_examples().iter() {
-                black_box(iref::IriRef::new(*iri).unwrap());
-            }
-        })
-    });
-}
-
-fn rel_examples() -> &'static [&'static str] {
-    &[
+fn rel_examples() -> impl Iterator<Item=&'static str> {
+    [
         "g:h",
         "g",
         "g/",
@@ -157,85 +76,176 @@ fn rel_examples() -> &'static [&'static str] {
         "g#s/../x",
         "http:g",
         "./g:h",
-    ]
+    ].iter().copied()
 }
 
-fn bench_oxiri_resolve(c: &mut Criterion) {
-    let base = Oxiri::parse("http://a/b/c/d;p?q").unwrap();
-
-    let mut buf = String::new();
-    c.bench_function("resolve oxiri", |b| {
+fn bench_parse(c: &mut Criterion) {
+    let mut group = c.benchmark_group("Parse IRI");
+    group.bench_function("iri-string", |b| {
         b.iter(|| {
-            for relative in rel_examples().iter() {
-                buf.clear();
-                black_box(base.resolve_into(relative, &mut buf).unwrap());
+            for iri in abs_examples() {
+                black_box(iri_string::types::IriStr::new(iri).unwrap());
             }
         })
     });
-}
-
-fn bench_sophia_resolve(c: &mut Criterion) {
-    let base = resolve::BaseIri::new("http://a/b/c/d;p?q").unwrap();
-
-    let mut buf = String::new();
-    c.bench_function("resolve sophia", |b| {
+    group.bench_function("iref", |b| {
         b.iter(|| {
-            for relative in rel_examples().iter() {
-                buf.clear();
-                black_box(base.resolve_into(*relative, &mut buf).unwrap());
+            for iri in abs_examples() {
+                black_box(iref::Iri::new(iri).unwrap());
             }
         })
     });
+    group.bench_function("oxiri", |b| {
+        b.iter(|| {
+            for iri in abs_examples() {
+                black_box(Oxiri::parse(iri).unwrap());
+            }
+        })
+    });
+    group.bench_function("sophia_iri", |b| {
+        b.iter(|| {
+            for iri in abs_examples() {
+                black_box(sophia_iri::Iri::new(iri).unwrap());
+            }
+        })
+    });
+    group.finish();
 }
 
-// This bench is much slower than the ones for oxiri and sophia,
-// because it allocates a new String on each test, while oxiri and sophia reuse the same buffer all along.
-//
-// This may be consider an unfair comparison, but then iref does not provide any way to reuse a buffer...
-fn bench_iref_resolve(c: &mut Criterion) {
-    let base = iref::Iri::new("http://a/b/c/d;p?q").unwrap();
-
-    c.bench_function("resolve iref", |b| {
+fn bench_parse_ref_abs(c: &mut Criterion) {
+    let mut group = c.benchmark_group("Parse IRI reference absolute");
+    group.bench_function("iri-string", |b| {
         b.iter(|| {
-            for relative in rel_examples().iter() {
+            for iriref in abs_examples() {
+                black_box(iri_string::types::IriReferenceStr::new(iriref).unwrap());
+            }
+        })
+    });
+    group.bench_function("iref", |b| {
+        b.iter(|| {
+            for iriref in abs_examples() {
+                black_box(iref::IriRef::new(iriref).unwrap());
+            }
+        })
+    });
+    group.bench_function("oxiri", |b| {
+        b.iter(|| {
+            for iriref in abs_examples() {
+                black_box(OxiriRef::parse(iriref).unwrap());
+            }
+        })
+    });
+    group.bench_function("sophia_iri", |b| {
+        b.iter(|| {
+            for iriref in abs_examples() {
+                black_box(sophia_iri::IriRef::new(iriref).unwrap());
+            }
+        })
+    });
+    group.finish();
+}
+
+fn bench_parse_ref_rel(c: &mut Criterion) {
+    let mut group = c.benchmark_group("Parse IRI reference relative");
+    group.bench_function("iri-string", |b| {
+        b.iter(|| {
+            for iriref in rel_examples() {
+                black_box(iri_string::types::IriReferenceStr::new(iriref).unwrap());
+            }
+        })
+    });
+    group.bench_function("iref", |b| {
+        b.iter(|| {
+            for iriref in rel_examples() {
+                black_box(iref::IriRef::new(iriref).unwrap());
+            }
+        })
+    });
+    group.bench_function("oxiri", |b| {
+        b.iter(|| {
+            for iriref in rel_examples() {
+                black_box(OxiriRef::parse(iriref).unwrap());
+            }
+        })
+    });
+    group.bench_function("sophia_iri", |b| {
+        b.iter(|| {
+            for iriref in rel_examples() {
+                black_box(sophia_iri::IriRef::new(iriref).unwrap());
+            }
+        })
+    });
+    group.finish();
+}
+
+fn bench_resolve(c: &mut Criterion) {
+    static BASE: &str = "http://a/b/c/d;p?q";
+
+    let mut group = c.benchmark_group("Resolve relative IRI reference against a fixed base IRI");
+
+    group.bench_function("iri-string", |b| {
+        let base = iri_string::types::IriAbsoluteStr::new(BASE).unwrap();
+        let resolver = iri_string::resolve::FixedBaseResolver::new(base);
+        b.iter(|| {
+            for relative in rel_examples() {
+                black_box(resolver.resolve(iri_string::types::IriReferenceStr::new(relative).unwrap()).to_dedicated_string());
+            }
+        })
+    });
+    group.bench_function("iref", |b| {
+        let base = iref::Iri::new(BASE).unwrap();
+        b.iter(|| {
+            for relative in rel_examples() {
                 let mut rel = iref::IriRefBuf::new(relative.to_string()).unwrap();
                 black_box(rel.resolve(base));
             }
         })
     });
-}
-
-// This bench is much slower than the ones for oxiri and sophia,
-// because it allocates a new String on each test, while oxiri and sophia reuse the same buffer all along.
-//
-// This may be consider an unfair comparison, but then iref does not provide any way to reuse a buffer...
-fn bench_iristring_resolve(c: &mut Criterion) {
-    let base = iri_string::types::IriAbsoluteStr::new("http://a/b/c/d;p?q").unwrap();
-    let resolver = iri_string::resolve::FixedBaseResolver::new(base);
-
-    c.bench_function("resolve iri-string", |b| {
+    group.bench_function("oxiri", |b| {
+        let base = Oxiri::parse(BASE).unwrap();
         b.iter(|| {
-            for relative in rel_examples().iter() {
-                black_box(resolver.resolve(iri_string::types::IriReferenceStr::new(*relative).unwrap()).to_dedicated_string());
+            for relative in rel_examples() {
+                let mut buf = String::new();
+                black_box(base.resolve_into(relative, &mut buf).unwrap());
             }
         })
     });
+    group.bench_function("sophia_iri", |b| {
+        let base = sophia_iri::resolve::BaseIri::new(BASE).unwrap();
+        b.iter(|| {
+            for relative in rel_examples() {
+                let mut buf = String::new();
+                black_box(base.resolve_into(relative, &mut buf).unwrap());
+            }
+        })
+    });
+    group.bench_function("oxiri Mbuf", |b| {
+        let base = Oxiri::parse(BASE).unwrap();
+        let mut buf = String::new();
+        b.iter(|| {
+            for relative in rel_examples() {
+                buf.clear();
+                black_box(base.resolve_into(relative, &mut buf).unwrap());
+            }
+        })
+    });
+    group.bench_function("sophia_iri Mbuf", |b| {
+        let base = sophia_iri::resolve::BaseIri::new(BASE).unwrap();
+        let mut buf = String::new();
+        b.iter(|| {
+            for relative in rel_examples() {
+                buf.clear();
+                black_box(base.resolve_into(relative, &mut buf).unwrap());
+            }
+        })
+    });
+    group.finish();
 }
 
-criterion_group!(iri,
-    bench_iref_parse,
-    bench_iref_parse_relative,
-    bench_iristring_parse,
-    bench_iristring_parse_relative,
-    bench_oxiri_parse,
-    bench_oxiri_parse_relative,
-    bench_sophia_iri_parse,
-    bench_sophia_iri_parse_relative,
-
-    bench_iristring_resolve,
-    bench_iref_resolve,
-    bench_oxiri_resolve,
-    bench_sophia_resolve,
+criterion_group!(iri, 
+    bench_parse,
+    bench_parse_ref_abs,
+    bench_parse_ref_rel,
+    bench_resolve,
 );
-
 criterion_main!(iri);
